fb0ad66110e821a6548c312cb1b3414df02ab8a5
Bright Owusu <bright_owusu@outlook.com>
2023-02-17 00:11:54 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 
 /**
@@ -9,11 +8,8 @@
 
 int main(void)
 {
-int s = strlen("and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n");
 
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
-
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+
 }
