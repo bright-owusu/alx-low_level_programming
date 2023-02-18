11bbd062d69f03982275832a8f4de50eab1efec2
Bright Owusu <bright_owusu@outlook.com>
2023-02-18 00:51:14 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabets in lowercase with 'putchar'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+
+	return (0);
+}
