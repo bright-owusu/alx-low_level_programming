587198a22087aa29f42251fe4e5661902d85a8be
Bright Owusu <bright_owusu@outlook.com>
2023-03-15 20:47:48 +0000

    Completed task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array and initializes it with c
+ * @size: the size of the array
+ * @c: the char that initializes the array
+ *
+ * Return: NULL if size is 0 or fail, pointer to array on SUCCESS
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+	return (str);
+}
