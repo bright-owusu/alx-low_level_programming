887f035a8a482dbde3ced7585526fb386a57d97a
Bright Owusu <bright_owusu@outlook.com>
2023-03-16 23:17:59 +0000

    Completed task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory and returns a pointer to it
+ * @b: integer parameter
+ *
+ * Return: pointer on SUCCESS, 98 on FAILURE
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+
+	if (ptr == NULL)
+		return (98);
+
+	return (ptr);
+}
