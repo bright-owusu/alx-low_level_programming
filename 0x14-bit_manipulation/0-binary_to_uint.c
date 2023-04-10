ae7391c910b88c04a3840122ca5d522fc02a8e60
Bright Owusu <bright_owusu@outlook.com>
2023-04-10 00:29:16 +0000

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (int i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' & b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
 		res = res * 2 + (b[i] - '0');
