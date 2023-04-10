6c5f9514b29c03c894517bada7eb548fa366c413
Bright Owusu <bright_owusu@outlook.com>
2023-04-10 00:10:42 +0000

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
@@ -13,12 +13,20 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (int i = 0; b[i]; i++)
+	for (int i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] == '0')
+		{
+			res = (res << 1) | 0;
+		}
+		else if (b[i] == '1')
+		{
+			res = (res << 1) | 1;
+		}
+		else
+		{
 			return (0);
-
-		res = res * 2 + (b[i] - '0');
+		}
 	}
 
 	return (res);
