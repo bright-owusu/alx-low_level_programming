eb01ad74b2b612625fcc2fa958dc3b8353566abb
Bright Owusu <bright_owusu@outlook.com>
2023-04-10 00:56:42 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	for (i = (sizeof(unsigned long int) * 8) - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
